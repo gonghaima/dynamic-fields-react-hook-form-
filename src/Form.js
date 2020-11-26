f25ade77635c0da806b290264bd375c92778af18
Steven <gonghaima@hotmail.com>
2020-11-26 23:54:51 +1100

    dynamic fields

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
 import { Button, Form } from "semantic-ui-react";
 import { FieldSet } from "./FieldSet";
 import { NumberInput } from "./NumberInput"
@@ -9,6 +9,11 @@ const fieldWidth = 8;
 
 export const Recipe = () => {
     const { register, handleSubmit, errors, control } = useForm();
+
+    const { fields, append, remove } = useFieldArray({
+        name: "ingredients",
+        control
+    });
 
     const submitForm = formData => {
         console.log(formData);
@@ -42,7 +47,6 @@ export const Recipe = () => {
                             </ErrorMessage>
                         )}
                     </Form.Field>
-
                     <Form.Field width={fieldWidth} error={!!errors.amount}>
                         <label htmlFor="amount">Servings</label>
                         <Controller
@@ -50,22 +54,52 @@ export const Recipe = () => {
                             name="amount"
                             defaultValue={0}
                             rules={{ max: 10 }}
-                            render={({ value, onChange }) => <NumberInput id="amount" onChange={onChange} checked={value} />}
+                            render={props => <NumberInput id="amount" {...props} />}
                         />
                         {errors.amount && (
                             <ErrorMessage>Maximum number of servings is 10.</ErrorMessage>
                         )}
                     </Form.Field>
-
-
                 </FieldSet>
-
+                <FieldSet label="Ingredients">
+                    {fields.map((field, index) => {
+                        return (
+                            <Row key={field.id}>
+                                <Form.Field width={8}>
+                                    <label htmlFor={`ingredients[${index}].name`}>Name</label>
+                                    <input
+                                        type="text"
+                                        ref={register()}
+                                        name={`ingredients[${index}].name`}
+                                        id={`ingredients[${index}].name`}
+                                    />
+                                </Form.Field>
+                                <Form.Field width={6}>
+                                    <label htmlFor={`ingredients[${index}].amount`}>Amount</label>
+                                    <input
+                                        type="text"
+                                        ref={register()}
+                                        defaultValue={field.amount}
+                                        name={`ingredients[${index}].amount`}
+                                        id={`ingredients[${index}].amount`}
+                                    />
+                                </Form.Field>
+                                <Button type="button" onClick={() => remove(index)}>&#8722;</Button>
+                            </Row>
+                        );
+                    })}
+                    <Button
+                        type="button"
+                        onClick={() => append({ name: "", amount: "" })}
+                    >
+                        Add ingredient
+          </Button>
+                </FieldSet>
                 <Form.Field>
                     <Button>Save</Button>
                 </Form.Field>
             </Form>
-        </Container>
-    );
+        </Container>);
 };
 
 const Container = styled.div`
@@ -77,6 +111,17 @@ const Container = styled.div`
 const ErrorMessage = styled.span`
   font-size: 12px;
   color: red;
+`;
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  & > * {
+    margin-right: 20px !important;
+  }
+  .ui.button {
+    margin: 10px 0 0 8px;
+  }
 `;
 
 ErrorMessage.defaultProps = { role: "alert" };
