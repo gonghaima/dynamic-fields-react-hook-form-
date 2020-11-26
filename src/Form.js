2b5d292c5be846dd908ef163b02cac29e141ee09
Steven <gonghaima@hotmail.com>
2020-11-26 22:45:57 +1100

    submit form

--- src/Form.js
+++ src/Form.js
@@ -2,35 +2,42 @@ import React from "react";
 import styled from "@emotion/styled";
 import { Button, Form } from "semantic-ui-react";
 import { FieldSet } from "./FieldSet";
+import { useForm } from "react-hook-form";
 
 const fieldWidth = 8;
 
 export const Recipe = () => {
-    return (
-        <Container>
-            <h1>New recipe</h1>
-            <Form size="large">
-                <FieldSet label="Basics">
-                    <Form.Field width={fieldWidth}>
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" />
-                    </Form.Field>
-                    <Form.Field width={fieldWidth}>
-                        <label htmlFor="description">Description</label>
-                        <textarea name="description" id="description" />
-                    </Form.Field>
-                    <Form.Field width={fieldWidth}>
-                        <label htmlFor="amount">Servings</label>
-                        <input type="number" name="amount" id="amount" />
-                    </Form.Field>
-                </FieldSet>
+  const { register, handleSubmit } = useForm();
 
-                <Form.Field>
-                    <Button>Save</Button>
-                </Form.Field>
-            </Form>
-        </Container>
-    );
+  const submitForm = formData => {
+    console.log(formData);
+  };
+
+  return (
+    <Container>
+      <h1>New recipe</h1>
+      <Form size="large" onSubmit={handleSubmit(submitForm)}>
+        <FieldSet label="Basics">
+          <Form.Field width={fieldWidth}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" ref={register} />
+          </Form.Field>
+          <Form.Field width={fieldWidth}>
+            <label htmlFor="description">Description</label>
+            <textarea name="description" id="description" ref={register} />
+          </Form.Field>
+          <Form.Field width={fieldWidth}>
+            <label htmlFor="amount">Servings</label>
+            <input type="number" name="amount" id="amount" ref={register} />
+          </Form.Field>
+        </FieldSet>
+
+        <Form.Field>
+          <Button>Save</Button>
+        </Form.Field>
+      </Form>
+    </Container>
+  );
 };
 
 const Container = styled.div`
