9db888beea0aafbf8a803793e5bb37207f1fb0f1
Steven <gonghaima@hotmail.com>
2020-11-26 22:32:53 +1100

    form base

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,34 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { useForm } from "react-hook-form";
+import { Button, Form } from "semantic-ui-react";
+import { FieldSet } from "./FieldSet";
+
+const fieldWidth = 8;
 
 export const Recipe = () => {
     return (
         <Container>
             <h1>New recipe</h1>
+            <Form size="large">
+                <FieldSet label="Basics">
+                    <Form.Field width={fieldWidth}>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </Form.Field>
+                    <Form.Field width={fieldWidth}>
+                        <label htmlFor="description">Description</label>
+                        <textarea name="description" id="description" />
+                    </Form.Field>
+                    <Form.Field width={fieldWidth}>
+                        <label htmlFor="amount">Servings</label>
+                        <input type="number" name="amount" id="amount" />
+                    </Form.Field>
+                </FieldSet>
+
+                <Form.Field>
+                    <Button>Save</Button>
+                </Form.Field>
+            </Form>
         </Container>
     );
 };
@@ -13,4 +36,5 @@ export const Recipe = () => {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 25px 50px;
 `;
